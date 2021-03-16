fe383ca835cea608272ed46fa9da7d668705d7b3
PaulinaPacyna <pacyna1998@gmail.com>
2021-03-16 20:15:05 +0100

    classification titles

--- classification.py
+++ classification.py
@@ -1,10 +1,8 @@
 import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
-
 from network import Network
 
 PATH = os.path.join("data", "classification")
@@ -12,6 +10,7 @@ datasets = ["data.simple", "data.three_gauss"]
 sizes = [100, 500, 1000, 10000]
 N = 100
 fig, axs = plt.subplots(3, 4, figsize=(12, 8))
+fig.tight_layout(pad=4.0)
 for j, size in enumerate(sizes):
     data = pd.read_csv(os.path.join(PATH, f"{datasets[0]}.train.{size}.csv"))
     mesh = (
@@ -28,6 +27,7 @@ for j, size in enumerate(sizes):
     pred_mesh = np.argmax(MLP.fit(mesh), axis=1)
     axs[0][j].scatter(mesh[:, 0], mesh[:, 1], s=0.5, c=pred_mesh)
     axs[0][j].scatter(data.x, data.y, c=data.cls, cmap="ocean", s=2)
+    axs[0][j].set_title(f"Network: {[2, 1]}, size: {size}")
 for j, size in enumerate(sizes):
     data = pd.read_csv(os.path.join(PATH, f"{datasets[1]}.train.{size}.csv"))
     mesh = (
@@ -44,6 +44,7 @@ for j, size in enumerate(sizes):
     pred_mesh = np.argmax(MLP.fit(mesh), axis=1)
     axs[1][j].scatter(mesh[:, 0], mesh[:, 1], s=0.5, c=pred_mesh)
     axs[1][j].scatter(data.x, data.y, c=data.cls, cmap="ocean", s=2)
+    axs[1][j].set_title(f"Network: {[2, 1]}, size: {size}")
 for j, size in enumerate(sizes):
     data = pd.read_csv(os.path.join(PATH, f"{datasets[1]}.train.{size}.csv"))
     mesh = (
@@ -60,4 +61,5 @@ for j, size in enumerate(sizes):
     pred_mesh = np.argmax(MLP.fit(mesh), axis=1)
     axs[2][j].scatter(mesh[:, 0], mesh[:, 1], s=0.5, c=pred_mesh)
     axs[2][j].scatter(data.x, data.y, c=data.cls, cmap="ocean", s=2)
+    axs[2][j].set_title(f"Network: {[2, 4, 1]}, size: {size}")
 plt.show()
